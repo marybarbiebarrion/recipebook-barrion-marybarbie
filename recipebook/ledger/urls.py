8ec97e7124971f1df3af1b76eafd29325fe2eee0
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-14 23:50:50 +0800

    Updated recipe_detail.html and urls.py(in ledger app)

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,7 +3,7 @@ from .views import RecipeListView, RecipeDetailView
 
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 
