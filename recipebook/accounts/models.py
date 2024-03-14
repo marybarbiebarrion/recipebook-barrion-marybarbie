029b6cb0470a2ce7cf7f11c3e65abd44fa4722db
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-15 03:33:39 +0800

    made some changes in class Profile in models.py from the accounts app

--- recipebook/accounts/models.py
+++ recipebook/accounts/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     bio = models.TextField(validators=[MinLengthValidator(255)])
+
+    def __str__(self):
+        return self.user.username
