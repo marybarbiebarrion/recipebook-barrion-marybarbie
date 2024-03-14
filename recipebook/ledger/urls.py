5d3e68c164a32469711ea9edec7f3dcd7cb9ff52
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-14 23:41:52 +0800

    Updated urls.py in ledger app

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from .views import RecipeListView, RecipeDetailView
+
 
 urlpatterns = [
-    path('recipes/list', recipe_list, name='recipe-list'),
-    path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
+
 
 app_name = "ledger"
