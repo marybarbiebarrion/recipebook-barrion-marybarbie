309cdac12b61fd0129dea4f8e3e1861174f79342
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-15 12:59:11 +0800

    added an ingredientadmin in admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Ingredient
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
@@ -7,4 +7,8 @@ class RecipeIngredientInline(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
