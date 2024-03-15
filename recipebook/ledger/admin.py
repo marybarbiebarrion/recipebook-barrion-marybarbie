8106b92396331c57d525fadd26b802a21c982409
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-15 13:03:35 +0800

    removed extra spaces in admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import Recipe, RecipeIngredient, Ingredient
 
+
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
